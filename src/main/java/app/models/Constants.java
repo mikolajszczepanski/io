3b2687acdd32a12963ba93fe5cc771f4b714895c
Nilvero <damians951@gmail.com>
2016-12-16 04:00:20 +0100

    magic numbers

--- src/main/java/app/models/Constants.java
+++ src/main/java/app/models/Constants.java
@@ -4,9 +4,19 @@ package app.models;
  */
 public final class Constants {
 	
-	
 	public static final String SPENDING = "spending";
-	
 	public static final String REVENUE = "revenue";
+	//Wymagane parametry rejstracji
+	public static final Integer usernameMinLength =5;
+	public static final Integer usernameMaxLength=20;
+	
+	public static final Integer passwordMinLength=5;
+	public static final Integer passwordMaxLength=40;
+	
+	public static final Integer emailMinLength=5;
+	public static final Integer emailMaxLength=40;
+	
+	public static final Integer answerMinLength=2;
+	public static final Integer answerMaxLength=40;
 	
 }
--- src/main/java/app/models/User.java
+++ src/main/java/app/models/User.java
@@ -136,23 +136,23 @@ public class User {
 	 */
 	public static List<String> Validate(String username,String password,String email,String answer) {
 		List<String> errors = new ArrayList<String>();
-		if (username == null || username.length() < 5 || username.length() > 20) {
-			errors.add("Nazwa użytkownika nie mieści się w przedziale [5, 20].");
+		if (username == null || username.length() < Constants.usernameMinLength || username.length() > Constants.usernameMaxLength) {
+			errors.add("Nazwa użytkownika nie mieści się w przedziale ["+Constants.usernameMinLength.toString()+", "+Constants.usernameMaxLength.toString()+"].");
 		}
-		if (password == null || password.length() < 5 || password.length() > 40) {
-			errors.add("Hasło nie mieści się w przedziale [5, 40].");
+		if (password == null || password.length() < Constants.passwordMinLength || password.length() > Constants.passwordMaxLength) {
+			errors.add("Hasło nie mieści się w przedziale ["+Constants.passwordMinLength.toString()+", "+Constants.passwordMaxLength.toString()+"].");
 		}
 		if (!password.matches(".*\\d+.*")) errors.add("Hasło musi zawierać co najmniej jedną cyfrę.");
 		if (password.equals(password.toLowerCase())) errors.add("Hasło musi zawierać co najmniej jedną dużą literę.");
 		if (password.equals(password.toUpperCase())) errors.add("Hasło musi zawierać co najmniej jedną małą literę.");
-		if (email == null || email.length() < 5 || email.length() > 40) {
-			errors.add("Email nie mieści się w przedziale [5, 40].");
+		if (email == null || email.length() < Constants.emailMinLength || email.length() > Constants.emailMaxLength) {
+			errors.add("Email nie mieści się w przedziale ["+Constants.emailMinLength.toString()+", "+Constants.emailMaxLength.toString()+"].");
 		}
 		if (email.indexOf('@') < 1 || email.indexOf('.') < 1 || new StringBuilder(email).reverse().toString().indexOf('.') > new StringBuilder(email).reverse().toString().indexOf('@')) {
 			errors.add("Podaj poprawny Email.");
 		}
-		if (answer == null || answer.length() < 2 || answer.length() > 40) {
-			errors.add("Odpowiedź nie mieści się w przedziale [2, 40].");
+		if (answer == null || answer.length() < Constants.answerMinLength || answer.length() > Constants.answerMaxLength) {
+			errors.add("Odpowiedź nie mieści się w przedziale ["+Constants.answerMinLength.toString()+", "+Constants.answerMaxLength.toString()+"].");
 		}
 		return errors;
 	}
